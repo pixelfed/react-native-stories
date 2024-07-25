a1f2842b80e656869bbf1a248f82117ce65f448b
Daniel Supernault <danielsupernault@gmail.com>
2024-07-25 03:34:30 -0600

    wip

--- src/core/helpers/storage.ts
+++ src/core/helpers/storage.ts
@@ -1,37 +1,26 @@
 /* eslint-disable global-require */
 import { STORAGE_KEY } from '../constants';
 import { ProgressStorageProps } from '../dto/helpersDTO';
+import { MMKV } from 'react-native-mmkv'
 
 export const clearProgressStorage = async () => {
 
   try {
-
-    const AsyncStorage = require( '@react-native-async-storage/async-storage' ).default;
-
-    return AsyncStorage.removeItem( STORAGE_KEY );
-
-  } catch ( error ) {
-
+    const storage = new MMKV()
+    return storage.delete(STORAGE_KEY)
+  } catch (error) {
     return null;
-
   }
 
 };
 
 export const getProgressStorage = async (): Promise<ProgressStorageProps> => {
-
   try {
-
-    const AsyncStorage = require( '@react-native-async-storage/async-storage' ).default;
-
-    const progress = await AsyncStorage.getItem( STORAGE_KEY );
-
-    return progress ? JSON.parse( progress ) : {};
-
-  } catch ( error ) {
-
+    const storage = new MMKV()
+    const progress = await storage.getString(STORAGE_KEY);
+    return progress ? JSON.parse(progress) : {};
+  } catch (error) {
     return {};
-
   }
 
 };
@@ -42,17 +31,11 @@ export const setProgressStorage = async ( user: string, lastSeen: string ) => {
   progress[user] = lastSeen;
 
   try {
-
-    const AsyncStorage = require( '@react-native-async-storage/async-storage' ).default;
-
-    await AsyncStorage.setItem( STORAGE_KEY, JSON.stringify( progress ) );
-
+    const storage = new MMKV()
+    await storage.set(STORAGE_KEY, JSON.stringify( progress ));
     return progress;
-
   } catch ( error ) {
-
     return {};
-
   }
 
 };
